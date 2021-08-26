4e6ab786fe4ebc0609646c7f4614b0af67821355
kudlav <vladankudlac@gmail.com>
2021-08-26 20:17:07 +0200

    Fix BUT portal domain change.

--- webscraper/src/main/java/cz/kudlav/vutindex/webscraper/util/UrlConstants.kt
+++ webscraper/src/main/java/cz/kudlav/vutindex/webscraper/util/UrlConstants.kt
@@ -1,8 +1,10 @@
 package cz.kudlav.vutindex.webscraper.util
 
-    const val INDEX_URL = "https://www.vutbr.cz/studis/student.phtml?sn=el_index"
-    const val LOGIN_URL = "https://www.vutbr.cz/login/intra"
-    const val REQUEST_URL = "https://www.vutbr.cz/login/in"
-    const val INTRA_URL = "https://www.vutbr.cz/intra"
-    const val KAM_URL = "https://www.vutbr.cz/external/kamb_redirect.php"
-    const val HEALTH_URL = "https://www.vutbr.cz/studis/student.phtml?sn=prohlaseni_studenta"
+const val INTRA_DOMAIN = "https://www.vut.cz/"
+
+const val INDEX_URL = INTRA_DOMAIN + "studis/student.phtml?sn=el_index"
+const val LOGIN_URL = INTRA_DOMAIN + "login/intra"
+const val REQUEST_URL = INTRA_DOMAIN + "login/in"
+const val INTRA_URL = INTRA_DOMAIN + "intra"
+const val KAM_URL = INTRA_DOMAIN + "external/kamb_redirect.php"
+const val HEALTH_URL = INTRA_DOMAIN + "studis/student.phtml?sn=prohlaseni_studenta"
